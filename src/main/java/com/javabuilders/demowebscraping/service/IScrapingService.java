15fe683e95bad466556653b9d65c7ef32370fa9d
Carlos <carlosalbertobarrioslopez9@gmail.com>
2024-11-20 20:47:46 -0600

    refactor: modificar interfaz ScrapingService

--- src/main/java/com/javabuilders/demowebscraping/service/IScrapingService.java
+++ src/main/java/com/javabuilders/demowebscraping/service/IScrapingService.java
@@ -1,6 +1,4 @@
 package com.javabuilders.demowebscraping.service;
-
-
 import com.javabuilders.demowebscraping.model.ScrapingParameters;
 import com.javabuilders.demowebscraping.model.ScrapingResult;
 
@@ -9,13 +7,11 @@ import com.javabuilders.demowebscraping.model.ScrapingResult;
  * Implementado por la clase {@link ScrapingService}.
  */
 public interface IScrapingService {
-
     /**
      * Realiza el scraping de productos a partir de los parámetros proporcionados.
      *
      * @param scrapingParameters Los parámetros necesarios para realizar el scraping (ej. URL de la página).
      * @return Un objeto {@link ScrapingResult} que contiene la lista de productos obtenidos.
      */
-    ScrapingResult performScraping(ScrapingParameters scrapingParameters);
-
+    ScrapingResult getFinalList(ScrapingParameters scrapingParameters);
 }
